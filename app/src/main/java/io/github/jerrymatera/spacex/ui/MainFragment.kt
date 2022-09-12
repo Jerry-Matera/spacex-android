ac8d67adf362ad94e40a703a56c6954d85c21b52
JerryMatera <jerryonyango8@gmail.com>
2022-09-12 22:33:37 +0300

    Setup ViewBinding

--- app/src/main/java/io/github/jerrymatera/spacex/ui/MainFragment.kt
+++ app/src/main/java/io/github/jerrymatera/spacex/ui/MainFragment.kt
@@ -6,15 +6,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import io.github.jerrymatera.spacex.R
+import io.github.jerrymatera.spacex.databinding.FragmentMainBinding
 
 
 class MainFragment : Fragment() {
+    private var _binding: FragmentMainBinding? = null
+    private val binding
+        get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
--- app/src/main/java/io/github/jerrymatera/spacex/ui/SpaceShipModalFragment.kt
+++ app/src/main/java/io/github/jerrymatera/spacex/ui/SpaceShipModalFragment.kt
@@ -6,15 +6,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import io.github.jerrymatera.spacex.R
+import io.github.jerrymatera.spacex.databinding.FragmentSpaceShipModalBinding
 
 class SpaceShipModalFragment : Fragment() {
+    private var _binding: FragmentSpaceShipModalBinding? = null
+    private val binding
+        get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_space_ship_modal, container, false)
+    ): View {
+        _binding = FragmentSpaceShipModalBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
